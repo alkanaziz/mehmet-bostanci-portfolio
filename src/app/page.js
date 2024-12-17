08c341345396662e5f239643031367925b91e85f
Alkan Aziz <alkanaziz@gmail.com>
2024-12-17 13:55:17 +0100

    Update homepage image source from home_img.png to banner.png

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full">
-      <Image className="w-full" src="/home_img.png" alt="Bostanci Art" width={1500} height={2024} />
+      <Image className="w-full" src="/banner.png" alt="Bostanci Art" width={1500} height={2024} />
     </div>
   );
 }
