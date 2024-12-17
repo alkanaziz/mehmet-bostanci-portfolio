1707c089009b9c775b7dc5a0b79a0647871e2844
Alkan Aziz <alkanaziz@gmail.com>
2024-12-17 14:55:02 +0100

    Refactor Header component and create Nav component for improved navigation structure

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,46 +1,5 @@
-"use client";
-
 import Image from "next/image";
-import { usePathname } from "next/navigation";
-
 const Header = () => {
-  const pathname = usePathname();
-
-  const menuItems = [
-    {
-      title: "Home",
-      link: "/",
-    },
-    {
-      title: "Über mich",
-      link: "/ueber-mich",
-    },
-    {
-      title: "Werke",
-      link: "/werke",
-    },
-    {
-      title: "Workshop",
-      link: "/workshop",
-    },
-    {
-      title: "Shop",
-      link: "/shop",
-    },
-    {
-      title: "Aktuelle Ausstellung",
-      link: "/aktuelle-ausstellung",
-    },
-    {
-      title: "Neues",
-      link: "/neues",
-    },
-    {
-      title: "Kontakt",
-      link: "/kontakt",
-    },
-  ];
-
   return (
     <header className="flex w-full flex-col items-center justify-center">
       <div className="logo_header relative h-52 w-full bg-[url('/banner.png')] bg-cover bg-[center_top_40%]">
@@ -52,24 +11,6 @@ const Header = () => {
           height={200}
         />
       </div>
-      <nav className="flex w-full max-w-screen-lg items-center justify-center">
-        <ul className="flex w-full flex-col items-center justify-between gap-4 py-4 font-bold lg:flex-row">
-          {menuItems.map((item) => (
-            <li key={item.title}>
-              <a
-                href={item.link}
-                className={`rounded-md px-2 py-1 font-extrabold ${
-                  pathname.split("/")[1] === item.link.split("/")[1]
-                    ? "bg-black text-white"
-                    : ""
-                }`}
-              >
-                {item.title}
-              </a>
-            </li>
-          ))}
-        </ul>
-      </nav>
     </header>
   );
 };
---
+++ src/components/Nav.js
@@ -0,0 +1,64 @@
+"use client";
+
+import { usePathname } from "next/navigation";
+
+const Nav = () => {
+  const pathname = usePathname();
+
+  const menuItems = [
+    {
+      title: "Home",
+      link: "/",
+    },
+    {
+      title: "Über mich",
+      link: "/ueber-mich",
+    },
+    {
+      title: "Werke",
+      link: "/werke",
+    },
+    {
+      title: "Workshop",
+      link: "/workshop",
+    },
+    {
+      title: "Shop",
+      link: "/shop",
+    },
+    {
+      title: "Aktuelle Ausstellung",
+      link: "/aktuelle-ausstellung",
+    },
+    {
+      title: "Neues",
+      link: "/neues",
+    },
+    {
+      title: "Kontakt",
+      link: "/kontakt",
+    },
+  ];
+  return (
+    <nav className="sticky top-0 z-50 flex w-full items-center justify-center bg-white bg-opacity-80 shadow-xl">
+      <ul className="flex w-full max-w-screen-lg flex-col items-center justify-between gap-4 py-4 font-bold lg:flex-row">
+        {menuItems.map((item) => (
+          <li key={item.title}>
+            <a
+              href={item.link}
+              className={`rounded-md px-2 py-1 font-extrabold ${
+                pathname.split("/")[1] === item.link.split("/")[1]
+                  ? "bg-black text-white"
+                  : ""
+              }`}
+            >
+              {item.title}
+            </a>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
+};
+
+export default Nav;
