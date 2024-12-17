81590a6afa87d01fffc9a9608ccd56ce518fd285
Alkan Aziz <alkanaziz@gmail.com>
2024-12-17 17:43:35 +0100

    Adjust Header component layout for improved responsiveness and visual consistency

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import Image from "next/image";
 const Header = () => {
   return (
     <header className="flex w-full flex-col items-center justify-center">
-      <div className="logo_header relative h-52 w-full bg-[url('/banner.png')] bg-cover bg-[center_top_40%]">
+      <div className="logo_header relative h-20 md:h-52 w-full bg-[url('/banner.png')] bg-cover bg-[center_top_40%]">
         <Image
-          className="absolute bottom-0 lg:left-[200px]"
+          className="absolute w-56 md:w-[500px] bottom-0 lg:left-[200px]"
           src="/logo_header.png"
           alt="Bostanci Art"
           width={500}
