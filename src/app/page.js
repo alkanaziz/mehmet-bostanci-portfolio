aff02b4888e294d5ff094a04435cfdc3900fc3b1
Alkan Aziz <alkanaziz@gmail.com>
2024-12-17 11:07:45 +0100

    Update Image component dimensions for improved display quality

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     <div className="w-full">
-      <Image className="w-full" src="/home_img.png" alt="Bostanci Art" width={1200} height={1500} />
+      <Image className="w-full" src="/home_img.png" alt="Bostanci Art" width={1500} height={2024} />
     </div>
   );
 }
