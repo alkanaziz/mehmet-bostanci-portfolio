91fd4ad0dd98d8dcd2072d51bcaa5f9f323306a9
Alkan Aziz <alkanaziz@gmail.com>
2024-12-20 11:35:06 +0100

    Wrap logo image in Link component for improved navigation in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,15 +1,18 @@
 import Image from "next/image";
+import Link from "next/link";
 const Header = () => {
   return (
     <header className="flex w-full flex-col items-center justify-center">
-      <div className="logo_header relative h-20 md:h-52 w-full bg-[url('/banner.png')] bg-cover bg-[center_top_40%]">
-        <Image
-          className="absolute w-56 md:w-[500px] bottom-0 lg:left-[200px]"
-          src="/logo_header.png"
-          alt="Bostanci Art"
-          width={500}
-          height={200}
-        />
+      <div className="logo_header relative h-20 w-full bg-[url('/banner.png')] bg-cover bg-[center_top_40%] md:h-52">
+        <Link href="/">
+          <Image
+            className="absolute bottom-0 w-56 md:w-[500px] lg:left-[200px]"
+            src="/logo_header.png"
+            alt="Bostanci Art"
+            width={500}
+            height={200}
+          />
+        </Link>
       </div>
     </header>
   );
