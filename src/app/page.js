23b611e54ae25ef0456c9ad0d12c69defba63e1c
Alkan Aziz <alkanaziz@gmail.com>
2024-12-23 17:01:29 +0100

    Add blurred placeholder for banner image to enhance loading experience

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,27 @@
 import Image from "next/image";
+import addBlurredDataUrl from "@/lib/getBase64";
+import path from "path";
 
-export default function Home() {
+export default async function Home() {
+  const photo = {
+    src: {
+      large: "/banner.png"
+    }
+  };
+
+  const photoWithBlur = await addBlurredDataUrl(photo, process.cwd() + "/public");
+
   return (
     <div className="w-full">
-      <Image className="w-full" src="/banner.png" alt="Bostanci Art" width={1500} height={2024} />
+      <Image
+        className="w-full"
+        src="/banner.png"
+        alt="Bostanci Art"
+        width={1500}
+        height={2024}
+        placeholder="blur"
+        blurDataURL={photoWithBlur.blurredDataUrl}
+      />
     </div>
   );
 }
