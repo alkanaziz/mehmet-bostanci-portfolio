06b958b94d70fac1d0e9a6b6f0575b6b01f9714f
Alkan Aziz <alkanaziz@gmail.com>
2024-12-23 19:52:10 +0100

    Update Header component logo position for improved layout

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
       <div className="logo_header relative h-20 w-full bg-[url('/banner.png')] bg-cover bg-[center_top_40%] md:h-52">
         <Link href="/">
           <Image
-            className="absolute bottom-0 w-56 md:w-[500px] lg:left-[200px]"
+            className="absolute bottom-0 w-56 md:w-[500px] lg:left-[12px]"
             src="/logo_header.png"
             alt="Bostanci Art"
             width={500}
