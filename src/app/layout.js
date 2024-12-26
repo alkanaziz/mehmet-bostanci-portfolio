45960e6283ae65cc2f940e080d25927b70c01b04
Alkan Aziz <alkanaziz@gmail.com>
2024-12-26 16:52:36 +0100

    Remove Sidebar component from RootLayout

--- src/app/layout.js
+++ src/app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <Nav />
-        <Sidebar />
+        {/* <Sidebar /> */}
         <Breadcrumb />
         {children}
         <Footer />
