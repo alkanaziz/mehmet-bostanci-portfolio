29262ba54035b6e36f297b727da9b8fc1f12ec0a
Alkan Aziz <alkanaziz@gmail.com>
2025-01-16 14:25:07 +0100

    Enhance Gallery component: add scroll to top on topic change and improve JSX formatting

--- src/components/Gallery.js
+++ src/components/Gallery.js
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import ImgContainer from "./ImgContainer";
 import { useEffect, useState } from "react";
@@ -16,6 +16,10 @@ export default function Gallery({ topic }) {
   const [error, setError] = useState(null);
 
   useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [topic]);
+
+  useEffect(() => {
     const loadImages = async () => {
       try {
         const data = await fetchImages(topic);
@@ -24,27 +28,27 @@ export default function Gallery({ topic }) {
         setError(err);
       }
     };
-    
+
     loadImages();
   }, [topic]);
 
   if (error) {
-    return <h2 className="m-4 text-2xl-font-bold">Kein Bild gefunden!</h2>;
+    return <h2 className="text-2xl-font-bold m-4">Kein Bild gefunden!</h2>;
   }
 
   if (!images) {
     return (
-      <div className="flex justify-center items-center h-96">
-        <div className="animate-spin rounded-full h-12 w-12 border-b-2 border-gray-900"></div>
+      <div className="flex h-96 items-center justify-center">
+        <div className="h-12 w-12 animate-spin rounded-full border-b-2 border-gray-900"></div>
       </div>
     );
   }
 
   return (
-    <div className="my-3 mx-10 flex flex-col gap-3">
+    <div className="mx-10 my-3 flex flex-col gap-3">
       {Object.entries(images).map(([prefix, photos]) => (
         <div key={prefix}>
-          <div className="flex flex-wrap justify-center sm:justify-end gap-3">
+          <div className="flex flex-wrap justify-center gap-3 sm:justify-end">
             {photos.map((photo) => (
               <ImgContainer key={photo.id} photo={photo} />
             ))}
