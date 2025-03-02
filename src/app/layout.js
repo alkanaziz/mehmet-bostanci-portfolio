b1de556250b36f6eb5b3c02fb765267c252d9378
Alkan Aziz <alkanaziz@gmail.com>
2025-03-02 23:50:05 +0100

    Remove commented-out Sidebar component from layout for cleaner code

--- src/app/layout.js
+++ src/app/layout.js
@@ -29,7 +29,6 @@ export default function RootLayout({ children }) {
       >
         <Header />
         <Nav />
-        {/* <Sidebar /> */}
         <Breadcrumb />
         {children}
         <Footer />
